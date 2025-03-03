3ab7ed021f29181d8e50c4bcec41886991aad180
Aaisma Wagle <98641051+Aaisma@users.noreply.github.com>
2025-03-04 00:01:49 +0545

    Add files via upload

--- nested_if.c
+++ nested_if.c
@@ -1,24 +1,29 @@
-#include<stdio.h> //to be checked!
+#include <stdio.h>
 
-int main(){
+int main() {
     int num;
 
-    printf("Enter your marks");
+    printf("Enter your marks out of 100: ");
     scanf("%d", &num);
 
-
-    if(num>=90 && num < 80){
-        print("You have scored A+.");
-    } else if(num>=80 && num < 70){
-        print("You have scored A.");
-
-    } else if (num>=70 && num < 60){
-        print("You have scored B+.");
-
+    // Ensure the input is within the valid range (0 to 100)
+    if (num < 0 || num > 100) {
+        printf("Invalid input. Please enter marks between 0 and 100.");
+    } else if (num >= 90) {
+        printf("You have scored A+.");
+    } else if (num >= 80) {
+        printf("You have scored A.");
+    } else if (num >= 70) {
+        printf("You have scored B+.");
+    } else if (num >= 60) {
+        printf("You have scored B.");
+    } else if (num >= 50) {
+        printf("You have scored C+.");
+    } else if (num >= 40) {
+        printf("You have scored C.");
     } else {
-        printf("You have failed");
+        printf("You have failed.");
     }
 
     return 0;
-
 }
